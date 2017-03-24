8e433da3634ff8a110c5a9b88a9f31ac3648f540
Evan <Evan@Evans-MBP-2.attlocal.net>
2017-03-24 17:52:29 -0500

    Completed exercise

--- mathAdderObj-C/ViewController.h
+++ mathAdderObj-C/ViewController.h
@@ -10,6 +10,18 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *numberOfTapsLbl;
+
+@property (weak, nonatomic) IBOutlet UITextField *numberToWin;
+
+@property (weak, nonatomic) IBOutlet UIImageView *tapLogo;
+
+@property (weak, nonatomic) IBOutlet UIButton *tapBtn;
+
+@property (weak, nonatomic) IBOutlet UILabel *howManyToWin;
+
+@property (weak, nonatomic) IBOutlet UIButton *playBtn;
+
 
 @end
 
--- mathAdderObj-C/ViewController.m
+++ mathAdderObj-C/ViewController.m
@@ -10,19 +10,72 @@
 
 @interface ViewController ()
 
+{
+   int maxTaps;
+   int numberOfTaps;
+}
+
+
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self startScreen];
+    
 }
 
+- (IBAction)playBtn:(id)sender {
+    
+    if (![self.numberToWin.text isEqualToString:@""]){
+        _tapBtn.hidden = NO;
+        _numberOfTapsLbl.hidden = NO;
+        _playBtn.hidden = YES;
+        _tapLogo.hidden = YES;
+        _numberToWin.hidden = YES;
+        _howManyToWin.hidden = YES;
+        self.numberOfTapsLbl.text = @"0 Taps";
+        
+        maxTaps = [self.numberToWin.text intValue];
+        
+    } else {
+        NSLog(@"Please Enter a Number");
+    }
+    
+    
+    
+    
+    
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)tapBtn:(id)sender {
+    
+    numberOfTaps += 1;
+    
+    if (numberOfTaps < maxTaps) {
+        _numberOfTapsLbl.text = [NSString stringWithFormat: @"%d Taps", numberOfTaps];
+    } else {
+        [self startScreen];
+        
+    }
+    
+}
+
+- (void) startScreen {
+    _tapBtn.hidden = YES;
+    _numberOfTapsLbl.hidden = YES;
+    _playBtn.hidden = NO;
+    _tapLogo.hidden = NO;
+    _numberToWin.hidden = NO;
+    _howManyToWin.hidden = NO;
+    _numberToWin.text = @"";
+    
+    maxTaps = 0;
+    numberOfTaps = 0;
 }
 
 
